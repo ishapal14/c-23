edeba123d5d8dddfb68b8ff8b9f24194b505ab40
ishapal14 <84769986+ishapal14@users.noreply.github.com>
2021-06-21 20:11:31 +0530

    Add files via upload

---
+++ c 23/PiratesInvasionStage-0.5-main/js/Tower.js
@@ -0,0 +1,26 @@
+class Tower{
+constructor(x, y, width, height){
+    var options = {
+      isStatic: true
+    }
+
+    this.width = width;
+    this.height = height;
+    this.image = loadImage("assets/tower.png")
+    this.body = Bodies.rectangle(x, y, this.width, this.height, options)
+    World.add(world, this.body)
+
+  }
+
+  display(){
+    var pos = this.body.position
+    push();
+    translate(pos.x, pos.y)
+    fill("brown")
+    imageMode(CENTER)
+    image(this.image, 0,0,this.width, this.height)
+    pop()
+  }
+
+}
+
